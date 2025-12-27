f575eb7a54389b5c9f6495d00fec8f515c34e8f0
Yan Aung Hein <yan@apmhelp.com>
2025-12-27 17:51:39 +0700

    initialize project

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="grid place-items-center h-screen">
+    <div class="font-black text-5xl">La Persona</div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'La Persona',
+      htmlAttrs: { lang: 'en' },
+    },
+  },
+  colorMode: {
+    preference: 'dark',
+  },
+})
