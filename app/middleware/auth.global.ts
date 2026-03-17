7bb30530fe243baf3cb67d24b267317c8e5bbfe2
Yan Aung Hein <yan@apmhelp.com>
2026-03-17 19:06:33 +0700

    protect thakhin route

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,14 +1,18 @@
-export default defineNuxtRouteMiddleware(async (from, to) => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const { data: session } = await authClient.useSession(useFetch);
-  const protectedPrefixes = [ROUTES.PLATFORM.ROOT, ROUTES.THAKHIN.ROOT];
-  const isProtected = protectedPrefixes.some((prefix) =>
-    from.path.startsWith(prefix)
-  );
+  const isPlatformRoute = to.path.startsWith(ROUTES.PLATFORM.ROOT);
+  const isThakhinRoute = to.path.startsWith(ROUTES.THAKHIN.ROOT);
+  const isProtected = isPlatformRoute || isThakhinRoute;
 
   if (!session.value && isProtected) {
     return navigateTo({
       path: ROUTES.SIGN_IN,
       query: { redirectTo: to.fullPath },
     });
+  }
+
+  const isAdmin = session.value?.user?.role === 'admin';
+  if (isThakhinRoute && !isAdmin) {
+    return navigateTo(`${ROUTES.SIGN_IN}?redirectTo=${to.fullPath}`);
   }
 });
